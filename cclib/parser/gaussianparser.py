30dc5979a9cb764edc9a34c725ff5afcd507a844
Eric Berquist <eric.berquist@gmail.com>
2017-12-10 14:04:15 -0500

    Gaussian: match for numerical (static) polarizability

--- cclib/parser/gaussianparser.py
+++ cclib/parser/gaussianparser.py
@@ -2342,7 +2342,7 @@ class Gaussian(logfileparser.Logfile):
 
         # Static polarizability (from `polar`), lower triangular
         # matrix.
-        if line[1:26] == "SCF Polarizability for W=":
+        if line[1:26] in ("SCF Polarizability for W=", "Isotropic polarizability="):
             self.hp_polarizabilities = True
             if not hasattr(self, "polarizabilities"):
                 self.polarizabilities = []
